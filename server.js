36a320bb8ec177bd428234a50013ddd1732d516c
Sarah S <stms15@mun.ca>
2023-08-21 20:37:45 -0230

    added api rputes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-// const routes = require("./controllers");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up sessions
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 300000 }, // 5 minutes
+};
+
+app.use(session(sess));
 
 // Create Handlesbars.js engine
 const hbs = exphbs.create();
@@ -13,7 +24,11 @@ const hbs = exphbs.create();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
 
 // Connect to sequelize and then listen
 sequelize.sync({ force: false }).then(() => {
